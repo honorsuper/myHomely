cfd48d4b3667080130a974e90a930ff27fd03494
honorsuper <honorsuper@126.com>
2023-12-19 21:38:48 +0800

    feat: 移动端提示

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <script setup lang="ts">
+import { computed } from 'vue'
 import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import { isMobile } from './utils'
+
+const flag = computed(() => {
+  return isMobile()
+})
 </script>
 
 <template>
@@ -10,8 +16,12 @@ import zhCN from 'ant-design-vue/es/locale/zh_CN'
         colorPrimary: '#6965db',
       },
     }"
-    ><RouterView
-  /></a-config-provider>
+  >
+    <div v-if="flag" class="bg-white h-full w-full flex justify-center items-center">
+      本应用暂不支持移动端，请在PC端打开！
+    </div>
+    <RouterView v-else />
+  </a-config-provider>
 </template>
 
 <style scoped></style>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,3 +5,9 @@ export const handleLogout = () => {
   localStorage.clear()
   location.href = location.origin + '/login'
 }
+
+//判断手机端还是PC端
+export const isMobile = () =>
+  navigator.userAgent.match(
+    /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i,
+  )
